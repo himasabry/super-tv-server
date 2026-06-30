26bfbca8af171ba9c440b73f581135eeaaa91144
himasabry <36902869+himasabry@users.noreply.github.com>
2026-07-01 01:44:52 +0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,6 +1,10 @@
 const STREAMS = {
-  "SUPER_TV_1": "https://restream-app.super-stv.workers.dev/live/15459447873244.m3u8",
-  "SUPER_TV_2": "https://restream-app.super-stv.workers.dev/live/15458583715548.m3u8"
+  "SUPER_TV_1": 
+    "https://restream-app.super-stv.workers.dev/live/15459447873244.m3u8",
+  "SUPER_TV_2": 
+    "https://restream-app.super-stv.workers.dev/live/15458583715548.m3u8",
+  "SUPER_TV_3": 
+    "https://live-sub-global-cdn-v02.sooplive.com/live-stmc-37/295232895/auth_master_playlist.m3u8?aid=.A32.pxqRXFPZNcY9Qg1.wu37JxfiiRJm-XKrCLFd6FZmd7F8nA_8MkQBT1n4s1bzEKujBTZQQcSmE1p-WY309ZbX8zuOA5y10Yw0dV4ux4VP66hBTAfO7_uVbG8fCCeO6dB3VBHzITNWGmm8Zi2Xc8xlO39T4ZmvYB8tuEIpmMQX4t7ulSOLvMkz8RpliuCIJQcMdEIlrodHseoNQN-ZAyScajis5Pm57c5ucAKsgQ"
 };
 
 export default {
